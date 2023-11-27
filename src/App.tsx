c7c095a5a8e1b7b03178604bc60c76d5b1917e1d
AnByoungJun <battlezon@gmail.com>
2023-11-28 01:04:42 +0900

    typescript 설정 및 실습

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+const App = () => {
+  return (
+    <>
+        <Circle bgColor="teal" />
+        <Circle bgColor="toamto" />
+    </>
+  );
+}
+
+export default App;
