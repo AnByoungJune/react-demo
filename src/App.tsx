a53f459d848d49e848559076dfaaadfa3bb9f064
AnByoungJun <battlezon@gmail.com>
2023-11-29 22:58:57 +0900

    event & typescript

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,29 @@ const Container = styled.div`
 const H1 = styled.h1`
   color: ${props => props.theme.textColor};
 `;
+
+interface DummyProps {
+    text: string;
+    otherThingHere?: boolean;
+}
+const Dummy = ({text, otherThingHere}:DummyProps) => {
+    return (
+        <H1>{text}</H1>
+    );
+}
+
 const App = () => {
+  const onClick = (event:React.FormEvent<HTMLButtonElement>) => {
+
+  }
+
   return (
       <div>
           <Container>
-              <H1>Protected</H1>
+              <Dummy text={"Dummy Text"} />
+            <form>
+              <button onClick={onClick}>click me</button>
+            </form>
           </Container>
       </div>
   );
