080966a2ca9a296a2cc72005a9c0b07de7fee224
AnByoungJun <battlezon@gmail.com>
2023-12-01 00:40:41 +0900

    createBrowserRouter 테스트

--- src/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-declare module 'styled-components' {
-    export interface DefaultTheme {
-        textColor: string;
-        bgColor: string;
-        btnColor: string;
-    }
-}
