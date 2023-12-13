6e0b22a05dcd7eed58690c2c588cb9bea11f9ce3
AnByoungJun <battlezon@gmail.com>
2023-12-13 23:49:48 +0900

    nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,20 @@ import {RouterProvider} from "react-router-dom";
 import {createGlobalStyle} from "styled-components";
 import router from "./router";
 
-const  GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3&display=swap');
-  /* http://meyerweb.com/eric/tools/css/reset/ 
-     v2.0 | 20110126
-     License: none (public domain)
-  */
-
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
   del, dfn, em, img, ins, kbd, q, s, samp,
   small, strike, strong, sub, sup, tt, var,
   b, u, i, center,
-  dl, dt, dd, ol, ul, li,
+  dl, dt, dd, menu, ol, ul, li,
   fieldset, form, label, legend,
   table, caption, tbody, tfoot, thead, tr, th, td,
   article, aside, canvas, details, embed,
   figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
+  main, menu, nav, output, ruby, section, summary,
   time, mark, audio, video {
     margin: 0;
     padding: 0;
@@ -29,31 +24,29 @@ const  GlobalStyle = createGlobalStyle`
     font: inherit;
     vertical-align: baseline;
   }
-
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
+  footer, header, hgroup, main, menu, nav, section {
     display: block;
   }
-
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+    display: none;
+  }
   body {
     line-height: 1;
   }
-
-  ol, ul {
+  menu, ol, ul {
     list-style: none;
   }
-
   blockquote, q {
     quotes: none;
   }
-
   blockquote:before, blockquote:after,
   q:before, q:after {
     content: '';
     content: none;
   }
-
   table {
     border-collapse: collapse;
     border-spacing: 0;
@@ -61,18 +54,19 @@ const  GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-  
   body {
-    font-family: 'Source_ Sans Pro', sans-serif;
-    background-color: ${(props) => props.theme.bgColor};
-    color: ${(props) => props.theme.textColor}
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color:${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor};
+    line-height: 1.2;
   }
-  
   a {
-    text-decoration: none;
-    color: inherit;
+    text-decoration:none;
+    color:inherit;
   }
 `;
+
 
 const App = () => {
   return(
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,8 @@ import {createBrowserRouter} from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 import Root from "./Root";
+import Price from "./routes/Price";
+import Chart from "./routes/Chart";
 
 const router = createBrowserRouter([
   {
@@ -14,7 +16,17 @@ const router = createBrowserRouter([
       },
       {
         path: '/:coinId',
-        element: <Coin />
+        element: <Coin />,
+        children: [
+          {
+            path: 'chart',
+            element: <Chart />,
+          },
+          {
+            path: 'price',
+            element: <Price />,
+          }
+        ]
       }
     ]
   }
